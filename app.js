83d2bd3a5c7961c3a01ed00208b897737beff4aa
Chau MAk Ka Ra <rab2014779@student.ctu.edu.vn>
2023-09-07 22:01:13 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const express = require('express')
-var cors = require('cors')
-const app = express()
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const contactsRouter = require('./app/routes/contact.route');
+const app = express();
 
-app.use(cors()) 
-app.use(express.json())
+
+app.use(cors()) ;
+app.use(express.json());
+
+
 
 app.get('/', function (req, res) {
-  res.json({message: " Welcome to contact book applications!!"})
+  res.json({message: " Welcome to contact book applications!!"});
 })
+
+app.use('/api/contacts', contactsRouter);
 
 module.exports = app;
